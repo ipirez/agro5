c0f3b8cb262a999449b323a90dfa7e4e4dbc3e80
ipirez <ipirez@palo-it.com>
2017-12-18 16:02:34 -0600

    alta producto ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,63 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//Auth
+import { AuthService } from './services/auth.service';
+import {AuthGuard} from './auth.guard'
+//pages
+import {LoginComponent} from './login/login.component'
+import {HomeComponent} from './home/home.component'
+import {ProductsComponent} from './products/products.component'
+import {DashboardComponent} from './dashboard/dashboard.component'
+import {AccountsComponent} from './accounts/accounts.component'
+import {ContactsComponent} from './contacts/contacts.component'
+import {FormsComponent} from './forms/forms.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'products',
+    component: ProductsComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'products/:steep',
+    component: ProductsComponent,
+    canActivate:[AuthGuard]
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'accounts',
+    component: AccountsComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'contacts',
+    component: ContactsComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'forms',
+    component: FormsComponent,
+    canActivate: [AuthGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers:[AuthService, AuthGuard]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+ }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  test: string = 'just a test';
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
+
   }
 
 }
